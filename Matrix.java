1c531ba63051cf85dab0d1940b02ce6a0494692f
MacKenzieColeman <macoleman@hartford.edu>
2023-11-09 15:36:06 -0500

    ints matrix + size declared

--- Matrix.java
+++ Matrix.java
@@ -1,2 +1,11 @@
 public class Matrix {
+  private int[][] matrix;
+  private int size;
+
+  public Matrix(int size) {
+    this.size = size;
+    this.matrix = new int[size][size];
+      System.out.println("Matrix dimensions: " + size + "x" + size);
+  }
+
 }
