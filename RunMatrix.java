3640a721f622f77dddf4078975e27be2dc11310b
MacKenzieColeman <macoleman@hartford.edu>
2023-11-09 15:43:16 -0500

    User input for the size, rejection output if less than 1

--- RunMatrix.java
+++ RunMatrix.java
@@ -2,5 +2,15 @@ import java.util.Scanner;
 
 public class RunMatrix {
   public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Enter the size of the matrix: ");
+    int size = scan.nextInt();
+    if(size < 1) {
+      System.out.println("That is too small to make an array! Input a positive number.");
+    }  else {
+    Matrix matrix = new Matrix(size);
+
+    }
+
   }
 }
